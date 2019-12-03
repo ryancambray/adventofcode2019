73cf1bebaa571d72a42e8fffb97ca5bc5e0df604
Ryan Cambray <ryan.cambray@sgdigital.com>
2019-12-03 09:56:20 +0000

    day 1

---
+++ adventofcodeday1.py
@@ -0,0 +1,19 @@
+import math
+
+def sum_fuel():
+	# Import file with mass list
+	with open("adventofcodeday1") as file:
+		total_fuel_needed = 0
+		for line in file:
+			total_fuel_needed = total_fuel_needed + calculate_fuel(int(line))
+	print("Fuel Needed = {}".format(total_fuel_needed))
+
+
+def calculate_fuel(mass):
+	fuel = mass / 3
+	fuel = math.floor(fuel)
+	fuel = fuel - 2
+	print("Fuel calculated from mass of '{}': {}".format(mass, fuel))
+	return fuel
+
+sum_fuel()
